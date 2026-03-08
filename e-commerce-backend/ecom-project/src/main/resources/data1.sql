4eed91711042f527a41aff3e8213f99b0cc09c6d
Abhishek-Abhi-1704 <abhibdk2004@gmail.com>
2026-03-08 12:49:25 +0530

    e-commerce project

---
+++ e-commerce-backend/ecom-project/src/main/resources/data1.sql
@@ -0,0 +1,4 @@
+INSERT INTO product(name, description,brand, price, category, release_Date, available, quantity)
+VALUES
+('motorola-edge 60 pro','one of the best phone','motorola',30000,'phone','2026-03-08',true,1),
+('samsung galaxy s26','best phone in the world','samsung',100000,'phone','2026-03-08',true,10);
